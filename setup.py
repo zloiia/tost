4de5e69cf43473708a3f0348d86c7198a9b2ccc5
df <fedin.da@gmail.com>
2022-09-30 09:09:04 +0300

    fgd

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='dap_classifier_data',
-    version='0.0.5',
+    version='0.0.6',
     packages=[
         'dap_classifier_data',
         'dap_classifier_data.*',
