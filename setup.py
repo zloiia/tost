ce6638ded72770f7b594b7e9c51daef6e63dee4f
df <fedin.da@gmail.com>
2022-09-30 09:08:49 +0300

    hj

--- setup.py
+++ setup.py
@@ -3,7 +3,10 @@ from distutils.core import setup
 setup(
     name='dap_classifier_data',
     version='0.0.5',
-    packages=['dap_classifier_data'],
+    packages=[
+        'dap_classifier_data',
+        'dap_classifier_data.*',
+        ],
     url='https://45.89.26.185:8081/class_dap_classifier/shared_data',
     license='MIT',
     author='d.fedin',
